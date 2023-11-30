261b4571d126a2d2838ac60b9b6e59770ed5e0af
Amwt24 <amawtachacha4@gmail.com>
2023-11-30 16:13:22 -0500

    Quitar las  cosas que estaban demas

--- MonitoreoPC.py
+++ MonitoreoPC.py
@@ -16,30 +16,22 @@ print("="*40, "System Information", "="*40)
 uname = platform.uname()
 print(f"System: {uname.system}")
 print(f"Node Name: {uname.node}")
-print(f"Processor: {uname.processor}")
+
 
 cpufreq = psutil.cpu_freq()
 print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
 print(f"Min Frequency: {cpufreq.min:.2f}Mhz")
 print(f"Current Frequency: {cpufreq.current:.2f}Mhz")
 
-print("CPU Usage Per Core:")
-for i, percentage in enumerate(psutil.cpu_percent(percpu=True)):
-    print(f"Core {i}: {percentage}%")
-print(f"Total CPU Usage: {psutil.cpu_percent()}%")
-#MEMORIA
+
+
 print("="*40, "Memory Information", "="*40)
 svmem = psutil.virtual_memory()
 print(f"Total: {get_size(svmem.total)}")
 print(f"Available: {get_size(svmem.available)}")
 print(f"Used: {get_size(svmem.used)}")
 print(f"Percentage: {svmem.percent}%")
-print("="*20, "SWAP", "="*20)
-swap = psutil.swap_memory()
-print(f"Total: {get_size(swap.total)}")
-print(f"Free: {get_size(swap.free)}")
-print(f"Used: {get_size(swap.used)}")
-print(f"Percentage: {swap.percent}%")
+
 
 print("="*40, "Disk Information", "="*40)
 print("Partitions and Usage:")
